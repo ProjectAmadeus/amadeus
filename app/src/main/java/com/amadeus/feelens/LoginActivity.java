37734a51a2a42dba9605a8031a366fcdb8e54695
Lucas Guilherme <lukeg299@gmail.com>
2019-03-20 11:19:30 -0300

    0.0.3
    - Added Firebase event listener to login (need to check);

--- app/src/main/java/com/amadeus/feelens/LoginActivity.java
+++ app/src/main/java/com/amadeus/feelens/LoginActivity.java
@@ -10,7 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
     /* Aqui também não alterei nada.
        Apenas copiei o código que recebi da última versão do projeto */
     boolean emailTrue = false, pwTrue = false;
@@ -18,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         final EditText etEmail = (EditText) findViewById(R.id.etEmail);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
@@ -27,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         final String logpassError = getResources().getString(R.string.empty_login_or_password);
         final TextView signUp = (TextView) findViewById(R.id.tvSignUp);
 
-        bLogin.setEnabled(false);
+
 
         signUp.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -45,6 +49,13 @@ public class LoginActivity extends AppCompatActivity {
                 Intent startMainFeed = new Intent(LoginActivity.this, MainActivity.class);
                 LoginActivity.this.startActivity(startMainFeed);
                 finish();
+
+                Bundle params = new Bundle();
+                params.putString("username", etEmail.getText().toString());
+                params.putString("password", etPassword.getText().toString());
+                mFirebaseAnalytics.logEvent("login_info", params);
+
+                bLogin.setEnabled(false);
             }
         });
 
@@ -114,6 +125,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
 
 
 
